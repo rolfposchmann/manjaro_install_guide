4d693ecd5ceed42f1a2984b89a1eeefee2997993
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-07-31 16:17:25 +0200

    Update zsh_config.sh

--- zsh_config.sh
+++ zsh_config.sh
@@ -11,7 +11,7 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias l="ls -la"
 
-#Tilix
+#Tilix fix: https://gnunn1.github.io/tilix-web/manual/vteconfig/
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
         source /etc/profile.d/vte.sh
 fi
