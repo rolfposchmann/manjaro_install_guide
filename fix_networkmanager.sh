85a3f811a512c63004269f10f94968732aa7830a
Rolf Poschmann <51391792+rolfposchmann@users.noreply.github.com>
2020-08-13 22:36:34 +0200

    Update fix_networkmanager.sh

--- fix_networkmanager.sh
+++ fix_networkmanager.sh
@@ -6,3 +6,10 @@ nano /etc/nsswitch.conf
  
 passwd: files mymachines
 group: files mymachines
+
+
+#fix network manager bug: https://forum.manjaro.org/t/every-other-reboot-i-have-no-networking-and-a-lot-of-unexpected-error-response-from-getnameowner/144416/10
+systemctl disable org.cups.cupsd.service
+systemctl enable org.cups.cupsd.socket
+
+See https://github.com/systemd/systemd/issues/15316#issuecomment-667658293 2
